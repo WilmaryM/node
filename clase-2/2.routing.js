6f7a4e7234b6b273b1d420f93c0256e78726df7c
Wilmary <wilmarymercedes1404@gmail.com>
2024-11-12 11:39:33 -0400

    clase 2 lista

--- clase-2/2.routing.js
+++ clase-2/2.routing.js
@@ -12,19 +12,41 @@ const processRequest = (req, res) => {
         case '/pokemon/ditto':
 
           res.setHeader('Content-Type', 'application/json; charset=utf-8')
-          return res.send(JSON.stringify(dittoJSON))
+          return res.end(JSON.stringify(dittoJSON))
         default:
           res.statusCode = 404
           res.setHeader('Content-Type', 'text/html; charset=utf-8')
           return res.end('<h1>404</h1>')
       }
 
-    case 'POST':
-      switch (url) {
-        case '/pokemon': {
-          const body = ''
+
+      case 'POST':
+        switch (url) {
+          case '/pokemon': {
+            let body = ''
+  
+            // escuchar el evento data
+            req.on('data', chunk => {
+              body += chunk.toString()
+            })
+  
+            req.on('end', () => {
+              const data = JSON.parse(body)
+              // llamar a una base de datos para guardar la info
+              res.writeHead(201, { 'Content-Type': 'application/json; charset=utf-8' })
+  
+              data.timestamp = Date.now()
+              res.end(JSON.stringify(data))
+            })
+  
+            break
+          }
+  
+          default:
+            res.statusCode = 404
+            res.setHeader('Content-Type', 'text/plain; charset=utf-8')
+            return res.end('404 Not Found')
         }
-      }
   }
 }
 const server = http.createServer(processRequest)
